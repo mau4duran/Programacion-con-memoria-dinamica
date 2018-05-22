2e8b9cbe708ac7f2b78f4de50640f45c8ce64ad5
mau4duran <mau4-duran@hotmail.com>
2018-05-22 12:13:41 -0500

    Update cifrado.c

--- cifrado.c
+++ cifrado.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int main(void) {
-    char texto[30];
+    char* texto;
     int lon;
 
     printf("Escriba Cadena\n");
@@ -16,15 +16,25 @@ int main(void) {
     int x;
 
     for (int i = 0; i < strlen(texto); i++) {
+
         if (texto[i]==' ' || texto[i]=='.' || texto[i]==',') {
             continue;
         }else{
-            texto[i]+=lon;
+            if (texto[i]>=65 && texto[i]<=90 && texto[i]+lon>90) {
+                texto[i]=65+(texto[i]+lon)-91;
+            } else if (texto[i]>=97 && texto[i]<=122 && texto[i]+lon>122) {
+                texto[i]=97+(texto[i]+lon)-123;
+            }
+            else{
+                texto[i]+=lon;
+            }
+
         }
     }
 
     for (int i = 0; i < strlen(texto); i++) {
         printf("%c", texto[i]);
     }
+    printf("\n");
 	return EXIT_SUCCESS;
 }
