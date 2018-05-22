cba02b7abcde119a715a30c690d304cc6d46f640
mau4duran <mau4-duran@hotmail.com>
2018-05-22 11:33:36 -0500

    Add files via upload

---
+++ cifrado.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(void) {
+    char* texto;
+    int lon;
+
+    printf("Escriba Cadena\n");
+    gets(texto);
+
+    printf("Longitud: ");
+    scanf("%d", &lon);
+
+    int x;
+
+    for (int i = 0; i < strlen(texto); i++) {
+        if (texto[i]==' ' || texto[i]=='.' || texto[i]==',') {
+            continue;
+        }else{
+            texto[i]+=lon;
+        }
+    }
+
+    for (int i = 0; i < strlen(texto); i++) {
+        printf("%c", texto[i]);
+    }
+	return EXIT_SUCCESS;
+}
